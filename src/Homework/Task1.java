8a6cb6975817b06e14258626a9f2acc3740a585b
Vera <inginer719@gmail.ru>
2023-07-08 10:30:26 -0400

    Initial commit

---
+++ src/Homework/Task1.java
@@ -0,0 +1,37 @@
+package Homework;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        // Tesk 2
+
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        String litter = "How are you?";
+        String book = "Java";
+
+        System.out.println(book + litter);
+        System.out.println("Сумма цифр числа: " + " a + b" +" равна " + sum);
+
+        // Task 3
+
+        int d = 12;
+        int k = 3;
+        int a1 = d + k;
+        int a2 = d - k;
+        int a3 = d * k;
+        int a4 = d / k;
+        int a5 = d % k;
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "Ideal Husband";
+        String text2 = "Crime and punishment";
+        String text3 = text + text2;
+
+        int number1 = 12;
+        int number2 = 14;
+        int sum = number2 - number1;
+
+
+        System.out.println(text3);
+        System.out.println(sum);
+
+    }
+}
